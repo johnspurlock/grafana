6dbb2930afc1b08b192fca38bdfef7ecefcc4dda
Virginia Cepeda <virginia.cepeda@grafana.com>
2023-06-14 15:50:24 -0300

    Show annotation fields in the right order

--- public/app/features/alerting/unified/utils/rule-form.ts
+++ public/app/features/alerting/unified/utils/rule-form.ts
@@ -96,6 +96,26 @@ function listifyLabelsOrAnnotations(item: Labels | Annotations | undefined): Arr
   return [...recordToArray(item || {}), { key: '', value: '' }];
 }
 
+//make sure default annotations are always shown in order even if empty
+function normalizeDefaultAnnotations(annotations: Array<{ key: string; value: string }>) {
+  const orderedAnnotations = [...annotations];
+  const defaultAnnotationKeys = defaultAnnotations.map((annotation) => annotation.key);
+
+  defaultAnnotationKeys.forEach((defaultAnnotationKey, index) => {
+    const fieldIndex = orderedAnnotations.findIndex((field) => field.key === defaultAnnotationKey);
+
+    if (fieldIndex === -1) {
+      //add the default annotation if abstent
+      const emptyValue = { key: defaultAnnotationKey, value: '' };
+      orderedAnnotations.splice(index, 0, emptyValue);
+    } else if (fieldIndex !== index) {
+      //move it to the correct position if present
+      orderedAnnotations.splice(index, 0, orderedAnnotations.splice(fieldIndex, 1)[0]);
+    }
+  });
+  return orderedAnnotations;
+}
+
 export function formValuesToRulerGrafanaRuleDTO(values: RuleFormValues): PostableRuleGrafanaRuleDTO {
   const { name, condition, noDataState, execErrState, evaluateFor, queries, isPaused } = values;
   if (condition) {
@@ -134,7 +154,7 @@ export function rulerRuleToFormValues(ruleWithLocation: RuleWithLocation): RuleF
         execErrState: ga.exec_err_state,
         queries: ga.data,
         condition: ga.condition,
-        annotations: listifyLabelsOrAnnotations(rule.annotations),
+        annotations: normalizeDefaultAnnotations(listifyLabelsOrAnnotations(rule.annotations)),
         labels: listifyLabelsOrAnnotations(rule.labels),
         folder: { title: namespace, id: ga.namespace_id },
         isPaused: ga.is_paused,
